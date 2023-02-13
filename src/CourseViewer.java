8841704c25ccd612cf2767ffc4ca96e7be903c7e
Soren Caraan <sorencaraan@outlook.com>
2023-02-13 17:21:52 -0500

    Added Main Method to test Appearance of GUI

--- src/CourseViewer.java
+++ src/CourseViewer.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class CourseViewer {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+        JFrame courseGUI = new CourseInterface();
+        courseGUI.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        courseGUI.setTitle("Course Enrollment Module");
+        courseGUI.setVisible(true);
+
     }
 }
